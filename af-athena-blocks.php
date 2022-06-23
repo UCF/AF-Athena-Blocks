6a457d9c6e7c0c2ad808e8d82970d28e5e0d9455
Mike Setzer <michaelsetzerucf@gmail.com>
2022-06-23 16:06:05 -0400

    Initial Commit

---
+++ af-athena-blocks.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: AF Athena Blocks
+Description: A plugin to add Athena elements to the Gutenberg Block Editor.
+Version: 0.0.1
+Author: UCF Admin & Finance IT
+License: GPL3
+GitHub Plugin URI: UCF/af-athena-blocks
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
